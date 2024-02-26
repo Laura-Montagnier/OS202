9b7c58051708fcb2bdae1b028f8aab07dad805f7
Laura-Montagnier <128493440+Laura-Montagnier@users.noreply.github.com>
2024-02-26 16:50:31 +0100

    Create reponses.c

---
+++ TravauxDiriges/TD_numero_1/reponses.c
@@ -0,0 +1,21 @@
+/*Réponses aux questions
+
+Question 1 :
+Pourquoi changer l'ordre des boucles change le temps d'exécution alors que la complexité reste la même ?
+
+C'est à cause de la complexité d'accès mémoire : de la hiérarchie des caches.
+C'est un problème qui n'existait pas quand les processeurs n'avaient qu'un seul coeur.
+
+L'information est stockée en colonnes, c'est donc plus rapide de parcourir les colonnes plutôt que les lignes :
+parcourir les lignes implique un chemin NON CONTIGU, alors que parcourir les colonnes implique un chemin CONTIGU.
+Il faut donc maximiser le parcours par colonnes :
+
+    pour j in colonne
+        pour k in colonne
+            pour i in ligne
+
+
+Question 2 :
+
+
+*/

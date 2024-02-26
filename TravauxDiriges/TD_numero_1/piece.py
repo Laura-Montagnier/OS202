11053ecde5aa4ef0e4c7eb8e4ae3b56e086d8998
Laura-Montagnier <128493440+Laura-Montagnier@users.noreply.github.com>
2024-02-26 20:32:00 +0100

    Create piece.py

---
+++ TravauxDiriges/TD_numero_1/piece.py
@@ -0,0 +1,43 @@
+from mpi4py import MPI
+
+# On initialise le MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+# On initialise le jeton
+jeton = None
+
+# On code pour le procédé zéro, qui doit en quelque sorte "initialiser"
+if rank == 0:
+    jeton = 1
+    
+    dest_rank = (rank + 1) % size #On travaille modulo le nombre de procédés.
+    comm.send(jeton, dest=dest_rank)
+    
+    jeton = comm.recv(source=size - 1)
+    
+    print(f"Le jeton est égal à {jeton}")
+    
+
+# Il faut coder l'action symétrique à l'envoi, c'est-à-dire la réception, 
+# pour tous les procédés non égaux à zéro.
+
+else:
+    # On reçoit le jeton du procédé précédent
+    source_rank = (rank - 1) % size
+    jeton = comm.recv(source=source_rank)
+    
+
+    print(f"Le jeton est égal à {jeton}")
+    
+    
+    jeton += 1
+    
+    
+    dest_rank = (rank + 1) % size
+    comm.send(jeton, dest=dest_rank)
+
+# On "ferme" MPI
+MPI.Finalize()
+
